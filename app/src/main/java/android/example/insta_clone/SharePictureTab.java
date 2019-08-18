0d42f26bc73eae4ea30bd5ae58da7b7f4e920223
aryan_0077 <rocking.am12459@gmail.com>
2019-08-18 10:25:26 +0530

    before migrating to androidx

---
+++ app/src/main/java/android/example/insta_clone/SharePictureTab.java
@@ -0,0 +1,31 @@
+package android.example.insta_clone;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SharePictureTab extends Fragment {
+
+
+    public SharePictureTab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_share_picture_tab, container, false);
+    }
+
+}
